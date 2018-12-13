df51678c7dbf33d54e8c66ca506793685d752ce6
Unknown <dancsa89@gmail.com>
2018-12-13 10:31:57 +0100

    enum inicialized

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -4,8 +4,15 @@ export class AppModel {
     public title?: string,
     public checked: boolean = false,
     public category?: string,
-    public text?: string
-  ) {
-
+    public text?: string,) {
+    Object.keys(Category);
   }
+  
+}
+
+export enum Category {  // ha nem adsz meg String értéket, akkor a tömbben elfoglalt helyét fogja visszadni
+  BILLING = 'BILLING', 
+  HOUSE = 'HOUSE', 
+  SHOPPING = 'SHOPPING', 
+  TRAVEL = 'TRAVEL'
 }
